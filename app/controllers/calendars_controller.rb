3aed8f08ea8af13a4869138c4670d2f38c81bae5
yk031000 <kanekoy03100@gmail.com>
2023-01-17 18:48:19 +0900

    Issue 6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,7 +36,7 @@ class CalendarsController < ApplicationController
       end
       
       wday_num = Date.today.wday+x
-      if wday_num <= 8
+      if wday_num >= 7
         wday_num = wday_num -7
       end
 
